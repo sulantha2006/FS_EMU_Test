0da7518c14bd854ad9a02bbf7b8bb0b5605d6802
sulantha <sulantha.s@gmail.com>
2021-07-22 11:38:50 -0700

    Fixed as per https://github.com/googleapis/python-firestore/issues/359#issuecomment-885143083

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from google.cloud import firestore
-from unittest import mock
+from unittest.mock import Mock
+
 from google.auth.credentials import Credentials
+from os import environ
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    dev_creds = mock.MagicMock(spec=Credentials)
-    db = firestore.Client(project="dev", credentials=dev_creds)
+    if environ.get("FIRESTORE_EMULATOR_HOST"):
+        print('EMULATOR CONNECTION')
+    dev_creds = Mock(spec=Credentials)
+    dev_creds.id_token = ""
+    db = firestore.Client(project="dummy-project", credentials=dev_creds)
     data = {u"user_id": 123, u"user_stuff": "slfk"}
 
     doc_ref = db.collection(u'testcollection').document()
     doc_ref.set(data)
+
     return "Hello World!"
